cb2590e5442fc08a70a0ea251a07a4cc8116a2ab
Sahil-Bhuker <150124018+Sahil-Bhuker@users.noreply.github.com>
2026-07-08 01:35:31 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (79.61%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -15,7 +15,7 @@ public:
             char ch = strs[0][i];
             for(int j = 1 ; j < strs.size(); j++){
                 if(strs[j][i] == ch) count++;
-                else break;
+                
             }
             if(count==strs.size()) ans.push_back(ch);
             else break;
