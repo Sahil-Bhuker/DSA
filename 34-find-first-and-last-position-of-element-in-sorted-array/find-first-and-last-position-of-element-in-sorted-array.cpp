eb6ac55c6beb6357fe9e893b59df1c410d4b4073
Sahil-Bhuker <150124018+Sahil-Bhuker@users.noreply.github.com>
2026-09-09 10:49:37 +0530

    Time: 3 ms (0.86%) | Memory: 17.7 MB (18.02%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,44 +1,31 @@
 class Solution {
 public:
-    int first(vector<int>& nums, int target){
-        int l = 0;
-        int r = nums.size()-1;
-        int ans =-1;
-        while(l<=r){
-            int mid = (r + l) / 2;
-            if(nums[mid]==target) {
-                ans = mid;
-                r = mid - 1;
-            }
-            else if(target>nums[mid]){
-                l = mid+1;
-            }else if(target<nums[mid]){
-                r = mid-1;
-            }
-        }
-        return ans;
-    }
-    int last(vector<int>& nums, int target){
-        int l = 0;
-        int r = nums.size()-1;
-        int ans =-1;
-        while(l<=r){
-            int mid = (r + l) / 2;
-            if(nums[mid]==target) {
-                ans = mid;
-                l = mid + 1;
-            }
-            else if(target>nums[mid]){
-                l = mid+1;
-            }else if(target<nums[mid]){
-                r = mid-1;
-            }
-        }
-        return ans;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int firstI = first(nums,target);
-        int lastI = last(nums,target);
-        return{firstI, lastI};
+        int n = nums.size();
+
+        int first = -1;
+        int second = -1;
+
+        int start = 0, end = n-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                first = mid;
+                end = mid - 1;
+            }else if(nums[mid]>target) end = mid - 1;
+            else start = mid + 1;
+        }
+
+        start = 0, end = n-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                second = mid;
+                start = mid + 1;
+            }else if(nums[mid]>target) end = mid - 1;
+            else start = mid + 1;
+        }
+
+        return {first,second};
     }
 };
