cb718d5cf802fefda84815801b19ab8928a19ed0
Sahil-Bhuker <150124018+Sahil-Bhuker@users.noreply.github.com>
2026-09-21 11:48:49 +0530

    Time: 2141 ms (5.33%) | Memory: 7.9 MB (21.15%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        int res = 0;
-        for(int i = 1; i<=num/2; i++){
+        if(num<=1) return false;
+        int res = 1;
+        for(int i = 2; i*1<num; i++){
             if(num%i==0){
                 res = res + i;
             }
